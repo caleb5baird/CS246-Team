11ff7c52ae48274b39529568a902febeb2513cae
Chase Haymond <hay14010@byui.edu>
2018-06-20 12:40:32 -0600

    Got the button to work just like the android tutorial we did

--- Cineman/app/src/main/java/com/bicodetech/cineman/MainActivity.java
+++ Cineman/app/src/main/java/com/bicodetech/cineman/MainActivity.java
@@ -1,10 +1,13 @@
 package com.bicodetech.cineman;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +15,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+
+    //I got this from the My First App androi tutorial
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         // Do something in response to button
+        Intent intent = new Intent(this, SearchResults.class);
+        EditText editText = (EditText) findViewById(R.id.searchBar);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
