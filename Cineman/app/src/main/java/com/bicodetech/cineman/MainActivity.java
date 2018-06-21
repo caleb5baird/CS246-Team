0fa65430949012bab57adf9ab5c7d0bd17546a59
Caleb Baird <caleb@bicodetech.com>
2018-06-21 12:26:46 -0600

    Merge with pull

--- Cineman/app/src/main/java/com/bicodetech/cineman/MainActivity.java
+++ Cineman/app/src/main/java/com/bicodetech/cineman/MainActivity.java
@@ -1,50 +1,31 @@
 package com.bicodetech.cineman;
 
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 public class MainActivity extends AppCompatActivity {
 
-    private Set<String> searchHistory;
-    private EditText etSearchBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        readSharedPreferences();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        writeSharedPreferneces();
-    }
+
+    //I got this from the My First App androi tutorial
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         // Do something in response to button
-        etSearchBar =(EditText) findViewById(R.id.searchBar);
-        searchHistory.add(etSearchBar.getText().toString());
-    }
-
-    private void writeSharedPreferneces(){
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = sp.edit();
-        editor.putStringSet("searchHistory", searchHistory);
-    }
-
-    private void readSharedPreferences() {
-        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
-        searchHistory = sp.getStringSet("searchHistory", null);
+        Intent intent = new Intent(this, SearchResults.class);
+        EditText editText = (EditText) findViewById(R.id.searchBar);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
--- Cineman/app/src/main/java/com/bicodetech/cineman/SearchResults.java
+++ Cineman/app/src/main/java/com/bicodetech/cineman/SearchResults.java
@@ -1,11 +1,13 @@
 package com.bicodetech.cineman;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class SearchResults extends AppCompatActivity {
 
@@ -13,17 +15,14 @@ public class SearchResults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
     }
 
 }
