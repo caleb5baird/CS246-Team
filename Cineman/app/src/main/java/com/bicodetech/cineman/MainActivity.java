0bddfde56286952f30b7dd1a8aa21db71d597f43
Sam Haymond <hay16009@byui.edu>
2018-06-28 11:35:45 -0600

    Shared Pref changes

--- Cineman/app/src/main/java/com/bicodetech/cineman/MainActivity.java
+++ Cineman/app/src/main/java/com/bicodetech/cineman/MainActivity.java
@@ -7,13 +7,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Set<String> searchHistory;
+    /*private Set<String> searchHistory;
+    private EditText etSearchBar;*/
+
     private EditText etSearchBar;
+    private static String SP_SEARCH_BAR = "com.bicodetech.searchBar";
+
+    /*@Override
+    protected void onPause() {
+        super.onPause();
+        writeSharedPreferneces();
+    }*/
 
     @Override
     protected void onPause() {
@@ -21,10 +31,18 @@ public class MainActivity extends AppCompatActivity {
         writeSharedPreferneces();
     }
 
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        readSharedPreferences();
+    }*/
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etSearchBar = findViewById(R.id.searchBar);
         readSharedPreferences();
     }
 
@@ -36,21 +54,35 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, SearchResults.class);
-        EditText editText = (EditText) findViewById(R.id.searchBar);
+        /*EditText editText = (EditText) findViewById(R.id.searchBar);
         String message = editText.getText().toString();
-        searchHistory.add(message);
-        intent.putExtra(EXTRA_MESSAGE, message);
+        searchHistory.add(message);*/
+        //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
-
 
     private void writeSharedPreferneces(){
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor editor = sp.edit();
-        editor.putStringSet("searchHistory", searchHistory);
+        editor.putString(SP_SEARCH_BAR, etSearchBar.getText().toString());
+        editor.apply();
     }
 
+    /*private void writeSharedPreferneces(){
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putStringSet("searchHistory", searchHistory);
+        editor.apply();
+    }*/
+
     private void readSharedPreferences() {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        String searchBar = sp.getString(SP_SEARCH_BAR, "");
+        etSearchBar.setText(searchBar);
+        Toast.makeText(this, "Shared Preferences Loaded", Toast.LENGTH_SHORT).show();
+    }
+
+    /*private void readSharedPreferences() {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
         searchHistory = sp.getStringSet("searchHistory", null);
         Intent intent = new Intent(this, SearchResults.class);
@@ -58,5 +90,5 @@ public class MainActivity extends AppCompatActivity {
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
-    }
+    }*/
 }
